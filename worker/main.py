21648254a3bdf0501f4218719c862fe927d89017
Matheus Aeroso <matheus.aeroso@tearsolucoes.com.br>
2020-07-30 00:57:57 -0300

    feat: enhance read csv speed

--- worker/main.py
+++ worker/main.py
@@ -1,6 +1,9 @@
+import numpy as np
 import pandas as pd
 
-g_csv = pd.read_csv('./files/g-1.txt', sep='\n', decimal=',', header=None)
-H_csv = pd.read_csv('./files/H-1/H-1.txt', header=None)
+g_csv = pd.read_csv('./files/g-1.txt', sep='\n', decimal=',', header=None, dtype=float)
+H_csv = pd.read_csv('./files/H-1/H-1.txt', header=None,dtype=float)
 
 H = H_csv.to_numpy()
+
+print('Cheguei aqui')
