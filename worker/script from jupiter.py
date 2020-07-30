efe8e8deb7969c127b006a7bd526c75611c0caa0
Lucas Giovanni de Castro Ribeiro <lucas.giovannicr@gmail.com>
2020-07-30 20:38:56 -0300

    Bugfix image

--- worker/script from jupiter.py
+++ worker/script from jupiter.py
@@ -13,17 +13,18 @@ H = pandas.read_csv('./files/H-1/H-1.txt',
                     header=None, dtype=float).to_numpy()
 
 
-r_old = pandas.read_csv('./files/avaliacao/sinal1.csv', sep='\n',
+g = pandas.read_csv('./files/avaliacao/sinal1.csv', sep='\n',
                         decimal=',', header=None, dtype=float).to_numpy()
+
+f_old = numpy.zeros_like(numpy.matmul(H.transpose(), g))
+
+r_old = g - numpy.matmul(H, f_old)
 
 
 p_old = numpy.matmul(H.transpose(), r_old)
 
 
-f_old = numpy.zeros_like(p_old)
-
-
-for index in range(6):
+for index in range(1):
 
     a_i = numpy.matmul(r_old.transpose(), r_old, dtype=float) / \
         numpy.matmul(p_old.transpose(), p_old)
@@ -47,10 +48,10 @@ end = time.time()
 print('Fim do precessamento da imagem -> Minutos: ' + str(int((end - start) / 60)) +
       '   Segundos: ' + str(int((end - start) / 60)))
 
-p_reshaped = p_next.reshape(60, 60)
+f_reshaped = f_next.reshape(60, 60)
 
 primeiraImagem = Image.fromarray(cv2.normalize(
-    p_reshaped, numpy.zeros_like(p_reshaped), 255, 0, cv2.NORM_MINMAX))
+    f_reshaped, numpy.zeros_like(f_reshaped), 255, 0, cv2.NORM_MINMAX))
 
 primeiraImagem.show()
 
