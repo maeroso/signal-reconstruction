f65ed2bfd0d6d34659718f5ffd6d8b53d58f5680
José Moscardi da Silva Junior <jmoscajr@gmail.com>
2020-07-31 01:20:04 -0300

    Implementação do segundo algoritmo e do calculo de convergencia

--- worker/script from jupiter.py
+++ worker/script from jupiter.py
@@ -1,20 +1,32 @@
 # To add a new cell, type '# %%'
 # To add a new markdown cell, type '# %% [markdown]'
 
+# %%
 import time
 import cv2
 from PIL import Image
 import numpy
 import pandas
+import math
 
-start = time.time()
+# start = time.time()
 
-H = pandas.read_csv('./files/H-1/H-1.txt',
+# %%
+
+H = pandas.read_csv('./../files/H-1/H-1.txt',
                     header=None, dtype=float).to_numpy()
 
+# %%
 
-g = pandas.read_csv('./files/avaliacao/sinal1.csv', sep='\n',
-                        decimal=',', header=None, dtype=float).to_numpy()
+g = pandas.read_csv('./../files/avaliacao/sinal2.csv', sep='\n',
+                    decimal=',', header=None, dtype=float).to_numpy()
+
+# %%
+erroMinimo = numpy.float64('1.0e-4')
+
+# %%
+
+start = time.time()
 
 f_old = numpy.zeros_like(numpy.matmul(H.transpose(), g))
 
@@ -24,7 +36,7 @@ r_old = g - numpy.matmul(H, f_old)
 p_old = numpy.matmul(H.transpose(), r_old)
 
 
-for index in range(1):
+while True:
 
     a_i = numpy.matmul(r_old.transpose(), r_old, dtype=float) / \
         numpy.matmul(p_old.transpose(), p_old)
@@ -38,20 +50,73 @@ for index in range(1):
 
     p_next = numpy.matmul(H.transpose(), r_next) + numpy.matmul(p_old, beta)
 
+    erro = numpy.absolute(numpy.linalg.norm(
+        r_next, ord=2) - numpy.linalg.norm(r_old, ord=2))
+
     p_old = p_next
 
     f_old = f_next
 
     r_old = r_next
 
+    if erro < erroMinimo:
+        break
+
+
 end = time.time()
-print('Fim do precessamento da imagem -> Minutos: ' + str(int((end - start) / 60)) +
-      '   Segundos: ' + str(int((end - start) / 60)))
+print('Fim do precessamento da imagem -> Minutos: ' +
+      str(int((end - start) / 60)) + '   Segundos: ' + str(int((end - start) % 60)))
 
 f_reshaped = f_next.reshape(60, 60)
 
 primeiraImagem = Image.fromarray(cv2.normalize(
-    f_reshaped, numpy.zeros_like(f_reshaped), 255, 0, cv2.NORM_MINMAX))
+    f_reshaped.transpose(), numpy.zeros_like(f_reshaped), 255, 0, cv2.NORM_MINMAX))
+
+primeiraImagem.show()
+
+
+# %%
+
+c = numpy.linalg.norm(numpy.matmul(H.transpose(), H), ord=2)
+
+# %%
+
+f_old = numpy.zeros_like(numpy.matmul(H.transpose(), g))
+
+y_old = f_old
+
+alfa_old = float(1)
+
+S = 794
+
+start = time.time()
+
+lmb = numpy.max(numpy.absolute(numpy.matmul(H.transpose(), g))) * 0.10
+
+for x in range(10):
+
+    f_next = math.log(S, lmb / c) * (y_old + numpy.matmul(
+        H.transpose() * (1/c), (g - numpy.matmul(H, y_old))))
+
+    alfa_next = (1 + math.sqrt(1 + 4 * math.pow(alfa_old, 2))) / 2
+
+    y_next = f_next + ((alfa_old - 1)/alfa_next) * (f_next - f_old)
+
+    f_old = f_next
+
+    alfa_old = alfa_next
+
+    y_old = y_next
+
+
+end = time.time()
+print('Fim do precessamento da imagem -> ' +
+      str(end - start))
+
+f_reshaped = f_next.reshape(60, 60)
+
+primeiraImagem = Image.fromarray(cv2.normalize(
+    f_reshaped.transpose(), numpy.zeros_like(f_reshaped), 255, 0, cv2.NORM_MINMAX))
 
 primeiraImagem.show()
 
