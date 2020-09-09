5b1f660466f8600c25d6eefd414790a16190796a
José Moscardi da Silva Junior <jmoscajr@gmail.com>
2020-09-09 17:18:33 -0300

    alteração da conversão do buffer e tratamento de pontos e vírgulas no cliente

--- worker/worker.py
+++ worker/worker.py
@@ -17,7 +17,7 @@ import json
 print(' [*] Carregando dados globais. Aguarde!')
 
 H = pandas.read_csv('./../files/H-1/H-1.txt',
-                     header=None, dtype=float).to_numpy()
+                    header=None, dtype=float).to_numpy()
 
 erro_minimo = numpy.float64('1.0e-4')
 
@@ -25,7 +25,6 @@ c = numpy.linalg.norm(numpy.matmul(H.transpose(), H), ord=2)
 
 
 def cgne(g):
-
     f_old = numpy.zeros_like(numpy.matmul(H.transpose(), g))
 
     r_old = g - numpy.matmul(H, f_old)
@@ -35,17 +34,17 @@ def cgne(g):
     while True:
 
         a_i = numpy.matmul(r_old.transpose(), r_old, dtype=float) / \
-            numpy.matmul(p_old.transpose(), p_old)
+              numpy.matmul(p_old.transpose(), p_old)
 
         f_next = f_old + p_old * a_i
 
         r_next = r_old - numpy.matmul(H, p_old) * a_i
 
         beta = numpy.divide(numpy.matmul(r_next.transpose(), r_next),
-                             numpy.matmul(r_old.transpose(), r_old))
+                            numpy.matmul(r_old.transpose(), r_old))
 
         p_next = numpy.matmul(H.transpose(), r_next) + \
-            p_old * beta
+                 p_old * beta
 
         erro = numpy.absolute(numpy.linalg.norm(
             r_next, ord=2) - numpy.linalg.norm(r_old, ord=2))
@@ -67,63 +66,61 @@ def cgne(g):
     primeira_imagem.show()
 
 
-# def funcao_s(sinal, index, limiar):
-#     if sinal >= 0:
-#         if sinal - limiar < 0:
-#             return 0
-#         else:
-#             return sinal - limiar
-#     else:
-#         if sinal + limiar >= 0:
-#             return 0
-#         else:
-#             return sinal + limiar
+def funcao_s(sinal, index, limiar):
+    if sinal >= 0:
+        if sinal - limiar < 0:
+            return 0
+        else:
+            return sinal - limiar
+    else:
+        if sinal + limiar >= 0:
+            return 0
+        else:
+            return sinal + limiar
 
 
-# def fast_iterative_shrinkage_thresjolding_algorithm(g):
+def fast_iterative_shrinkage_thresjolding_algorithm(g):
+    f_old = numpy.zeros_like(numpy.matmul(H.transpose(), g))
 
-#     f_old = numpy.zeros_like(numpy.matmul(H.transpose(), g))
+    y_old = f_old
 
-#     y_old = f_old
+    alfa_old = float(1)
 
-#     alfa_old = float(1)
+    lambda_value = numpy.max(numpy.absolute(
+        numpy.matmul(H.transpose(), g))) * 0.10
 
-#     lambda_value = numpy.max(numpy.absolute(
-#         numpy.matmul(H.transpose(), g))) * 0.10
+    limiar = numpy.absolute(lambda_value / c)
 
-#     limiar = numpy.absolute(lambda_value / c)
+    for x in range(5):
 
-#     for x in range(5):
+        f_next = y_old + numpy.matmul(
+            H.transpose() * (1 / c),
+            numpy.subtract(g, numpy.matmul(H, y_old))
+        )
 
-#         f_next = y_old + numpy.matmul(
-#             H.transpose() * (1/c),
-#             numpy.subtract(g, numpy.matmul(H, y_old))
-#         )
+        index = 0
 
-#         index = 0
+        for sinal in f_next:
+            f_next[index] = funcao_s(sinal, index, limiar)
 
-#         for sinal in f_next:
+            index += 1
 
-#             f_next[index] = funcao_s(sinal, index, limiar)
+        alfa_next = (1 + numpy.sqrt(1 + 4 * math.pow(alfa_old, 2))) / 2
 
-#             index += 1
+        y_next = f_next + ((alfa_old - 1) / alfa_next) * (f_next - f_old)
 
-#         alfa_next = (1 + numpy.sqrt(1 + 4 * math.pow(alfa_old, 2))) / 2
+        f_old = f_next
 
-#         y_next = f_next + ((alfa_old - 1)/alfa_next) * (f_next - f_old)
+        alfa_old = alfa_next
 
-#         f_old = f_next
+        y_old = y_next
 
-#         alfa_old = alfa_next
+    f_reshaped = f_next.reshape(60, 60)
 
-#         y_old = y_next
+    primeira_imagem = Image.fromarray(cv2.normalize(
+        f_reshaped.transpose(), numpy.zeros_like(f_reshaped), 255, 0, cv2.NORM_MINMAX))
 
-#     f_reshaped = f_next.reshape(60, 60)
-
-#     primeira_imagem = Image.fromarray(cv2.normalize(
-#         f_reshaped.transpose(), numpy.zeros_like(f_reshaped), 255, 0, cv2.NORM_MINMAX))
-
-#     primeira_imagem.show()
+    primeira_imagem.show()
 
 
 print(' [*] Abrindo canal de comunicação')
@@ -140,20 +137,12 @@ print(' [*] Aguardando por mensagens')
 def cgne_worker(ch, method, properties, body):
     print(" [x] Received message on cgne worker")
 
-    # decoded = body.decode()
+    string_array = numpy.array(body.decode().split(','))
 
-    g = numpy.fromstring(body)
-
-    npg = numpy.array(g)
-
-    print(npg)
-
-    # g = numpy.frombuffer(body, dtype=numpy.float32)
+    g = string_array.astype(numpy.float)
 
     # chamar o algoritmo passando G por parâmetro
-    cgne(npg)
-
-    # print(g)
+    cgne(g)
 
     print(" [x] Done - CGNE")
     ch.basic_ack(delivery_tag=method.delivery_tag)
@@ -162,10 +151,12 @@ def cgne_worker(ch, method, properties, body):
 def fista_worker(ch, method, properties, body):
     print(" [x] Received message on fista worker")
 
-    g = numpy.fromstring(body)
+    string_array = numpy.array(body.decode().split(','))
+
+    g = string_array.astype(numpy.float)
 
     # chamar o algoritmo passando G por parâmetro
-    # fast_iterative_shrinkage_thresjolding_algorithm(g)
+    fast_iterative_shrinkage_thresjolding_algorithm(g)
 
     print(" [x] Done - FISTA")
     ch.basic_ack(delivery_tag=method.delivery_tag)
