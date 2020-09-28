cb38f56c1eb9d0a62c0558cf85f248a4ad48bb6f
José Moscardi da Silva Junior <jmoscajr@gmail.com>
2020-09-28 02:11:03 -0300

    Correção da normalização da imagem e implementação do monitoramento de recursos

--- worker/worker.py
+++ worker/worker.py
@@ -2,9 +2,12 @@
 # To add a new markdown cell, type ' [markdown]'
 
 import json
+import sys
+import time
 
 import numpy
 import pika
+import psutil
 
 from cgne_thread import CgneThread
 from constants import Constants
@@ -15,7 +18,7 @@ from global_data import GlobalData
 class Worker:
 
     def worker_consume_queue(self, ch, method, properties, body):
-        print(" [X] Consuming worker queue")
+        sys.stdout.write(" [X] Consuming worker queue\n")
 
         decoded = body.decode()
 
@@ -25,21 +28,43 @@ class Worker:
             decoded.replace(',[""]', "")
         )
 
+        show_warning_message = False
+
+        overload = psutil.cpu_percent(percpu=False, interval=5) > 90
+
+        low_memory = psutil.virtual_memory().free < 600000000
+
+        while overload or low_memory:
+            if not show_warning_message:
+                if overload:
+                    sys.stdout.write(" [!] Unable to launch the thread, CPU load greater than 90%\n")
+                if low_memory:
+                    sys.stdout.write(" [!] Unable to start the thread, RAM remaining less than 600Mb\n")
+                show_warning_message = True
+
+            time.sleep(.800)
+
+            overload = psutil.cpu_percent(percpu=False, interval=5) > 90
+
+            low_memory = psutil.virtual_memory().free < 250000000
+
         if json_message['algorithmType'] == self.constants.cgne_algorithm_id:
             CgneThread(self.global_data, numpy.array(json_message['signalArray']))
-            print(" [*] CGNE thread was initiate")
+            sys.stdout.write(" [*] CGNE thread was initiate\n")
         else:
             FISTAThread(self.global_data, numpy.array(json_message['signalArray']))
-            print(" [*] FISTA thread was initiate")
+            sys.stdout.write(" [*] FISTA thread was initiate\n")
 
         ch.basic_ack(delivery_tag=method.delivery_tag)
+        sys.stdout.write(' [*] Waiting for messages\n')
 
     def __init__(self):
+
         self.constants = Constants()
 
-        self.global_data = GlobalData(False)
+        self.global_data = GlobalData()
 
-        print(' [*] Opening communication channel')
+        sys.stdout.write(' [*] Opening communication channel\n')
 
         self.connection = pika.BlockingConnection(
             pika.ConnectionParameters(host='localhost'))
@@ -47,7 +72,7 @@ class Worker:
 
         self.channel.queue_declare(queue=self.constants.worker_name_queue, durable=True)
 
-        print(' [*] Waiting for messages')
+        sys.stdout.write(' [*] Waiting for messages\n')
 
         self.channel.basic_qos(prefetch_count=1)
 
